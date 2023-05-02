c858b4a3a830f61695f87bd4152d5ebed9eddcaa
Lukykl1 <Lukykl1@gmail.com>
2023-05-02 23:45:30 +0200

    Update test_monthly.py

--- test_monthly.py
+++ test_monthly.py
@@ -16,11 +16,11 @@ station_id = sys.argv[1]
 df = df.loc[df['idStation'] == int(station_id)]
 
 os.mkdir(f"test_{station_id}")
-# group by month and year
+# Group by month and year
 df['month'] = df['timeStamp'].dt.month
 df['year'] = df['timeStamp'].dt.year
 
-#load all signals calculate mean and std of the signals and plot grouped by month and year
+# Load all signals calculate mean and std of the signals and plot grouped by month and year
 df['mean'] = df['idMeasurement'].apply(lambda x: np.load(f'data/{station_id}/{x}.npy').mean())
 df['std'] = df['idMeasurement'].apply(lambda x: np.load(f'data/{station_id}/{x}.npy').std())
 df['min'] = df['idMeasurement'].apply(lambda x: np.load(f'data/{station_id}/{x}.npy').min())
@@ -37,14 +37,12 @@ plt.title(f'Station {station_id}, Std')
 plt.savefig(f"test_{station_id}/_std.png")
 plt.clf()
 
-#absolute mean 
 df['abs_mean'] = df['idMeasurement'].apply(lambda x: np.abs(np.load(f'data/{station_id}/{x}.npy')).mean())
 df.groupby(['year', 'month']).mean()['abs_mean'].plot()
 plt.title(f'Station {station_id}, Abs Mean')
 plt.savefig(f"test_{station_id}/_abs_mean.png")
 plt.clf()
 
-#std of absolute values
 df['abs_std'] = df['idMeasurement'].apply(lambda x: np.abs(np.load(f'data/{station_id}/{x}.npy')).std())
 df.groupby(['year', 'month']).mean()['abs_std'].plot()
 plt.title(f'Station {station_id}, Abs Std')
@@ -61,7 +59,6 @@ plt.title(f'Station {station_id}, Max')
 plt.savefig(f"test_{station_id}/_max.png")
 plt.clf()
 
-#count
 df.groupby(['year', 'month']).count()['idMeasurement'].plot()
 plt.title(f'Station {station_id}, Count')
 plt.savefig(f"test_{station_id}/_count.png")
