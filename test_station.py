4ff76bc2ddbe463984eecc7e3fac42812044abd8
Lukykl1 <Lukykl1@gmail.com>
2023-05-02 23:44:49 +0200

    Update test_station.py

--- test_station.py
+++ test_station.py
@@ -3,10 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import os
-#description: script for testing single station
-#iterate 100 of data randomly
-#iterate 1% of data randomly
-#iterate 10 of data randomly with ['faultAnnotation'] == 1
+
+# Script for testing a single station
 
 # Load CSV file into a pandas DataFrame
 df = pd.read_csv('pdfaultNew.csv', delimiter=';')
@@ -18,9 +16,10 @@ df['timeStamp'] = pd.to_datetime(df['timeStamp'])
 station_id = sys.argv[1]
 df = df.loc[df['idStation'] == int(station_id)]
 
+# Create a new directory for the test results
 os.mkdir(f"test_{station_id}")
 
-# Iterate through each measurement and plot each 1000th sample
+# Iterate through each measurement and plot every 1000th sample
 for index, row in df[::1000].iterrows():
     measurement_id = row['idMeasurement']
     signal = np.load(f'data/{station_id}/{measurement_id}.npy')
@@ -29,7 +28,7 @@ for index, row in df[::1000].iterrows():
     plt.savefig(f"test_{station_id}//{measurement_id}.png")
     plt.clf()
 
-#iterate 1% of data randomly
+# Iterate through 1% of data randomly and plot each measurement
 for index, row in df.sample(frac=0.001).iterrows():
     measurement_id = row['idMeasurement']
     signal = np.load(f'data/{station_id}/{measurement_id}.npy')
@@ -38,7 +37,7 @@ for index, row in df.sample(frac=0.001).iterrows():
     plt.savefig(f"test_{station_id}//random_{measurement_id}.png")
     plt.clf()
 
-#iterate 10 of data randomly with ['faultAnnotation'] == 1
+# Iterate through 10 of data randomly with ['faultAnnotation'] == 1 and plot each measurement
 for index, row in df.loc[df['faultAnnotation'] == 1].sample(10).iterrows():
     measurement_id = row['idMeasurement']
     signal = np.load(f'data/{station_id}/{measurement_id}.npy')
@@ -47,21 +46,24 @@ for index, row in df.loc[df['faultAnnotation'] == 1].sample(10).iterrows():
     plt.savefig(f"test_{station_id}//fault_{measurement_id}.png")
     plt.clf()
 
+# If the second argument in command line is "stats"
 if sys.argv[2] == "stats":
-    #calculate metrics across all measurements (mean, std, min, max)
+    # Calculate metrics across all measurements (mean, std, min, max)
     df['mean'] = df['idMeasurement'].apply(lambda x: np.load(f'data/{station_id}/{x}.npy').mean())
     df['std'] = df['idMeasurement'].apply(lambda x: np.load(f'data/{station_id}/{x}.npy').std())
     df['min'] = df['idMeasurement'].apply(lambda x: np.load(f'data/{station_id}/{x}.npy').min())
     df['max'] = df['idMeasurement'].apply(lambda x: np.load(f'data/{station_id}/{x}.npy').max())
 
+    # Print the calculated metrics
     print("Mean", df['mean'].mean())
     print("Std", df['std'].mean())
     print("Min", df['min'].mean())
     print("Max", df['max'].mean())
 
-    #calculate outliers between samples using statistical methods
+    # Calculate outliers between samples using statistical methods
     df['outlier'] = df['idMeasurement'].apply(lambda x: np.load(f'data/{station_id}/{x}.npy'))
     df['outlier'] = df['outlier'].apply(lambda x: 1 if (x > (df['mean'].mean() + 3*df['std'].mean())).any() or (x < (df['mean'].mean() - 3*df['std'].mean())).any() else 0)
-    #print outliers
+
+    # Print the outliers
     print(df['outlier'].value_counts())
 
